bdaac112becd2de91bd2244d1a3d981e1f8850bd
headshigh <93933601+headshigh@users.noreply.github.com>
2022-12-23 18:09:11 +0545

    kldja

--- API/populate.js
+++
@@ -1,9 +0,0 @@
-const transaction = require("./dist/models/Transaction");
-const mongoose = require("mongoose");
-const del = async (req, res) => {
-  await mongoose.connect(
-    "mongodb+srv://nischal:720058726Nn1@cluster0.l67htts.mongodb.net/?retryWrites=true&w=majority"
-  );
-  const result = await transaction.deleteMany();
-};
-del();
