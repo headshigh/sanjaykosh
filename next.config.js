3d1f59dbb22c9aa8538bd719538eef57086b859d
headshigh <93933601+headshigh@users.noreply.github.com>
2022-12-23 18:15:17 +0545

    fkja

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  nextConfig,
+};
